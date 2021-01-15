ba590e40016c17571047b5c4b058400b11d6b03c
Marcin <bizior.marcin@googlemail.com>
2021-01-15 11:18:49 +0100

    code update

--- Modul_13/sql_todo/todo/main.py
+++ Modul_13/sql_todo/todo/main.py
@@ -25,36 +25,14 @@ def songs_list():
         return render_template("todo.html")
 
 
-@app.route("/delete/<int:song_id>", methods=["GET"])
+@app.route("/delete/<int:task_id>", methods=["GET"])
 def delete_task(task_id):
-    form = MusicLibrary()
-    error = ""
-    song = songs.delete(song_id - 1)
-    songs.save_all()
-    if not song:
-        abort(404)
-
-    return render_template("index.html", form=form, songs=songs.all(), error=error)
+    pass
 
 
-@app.route("/update/<int:song_id>/", methods=["GET", "POST"])
-def update_song(song_id):
-    song = songs.get(song_id - 1)
-    form = MusicLibrary(data=song)
-    error = ""
-
-    if request.method == "POST":
-        if form.validate_on_submit():
-            songs.update(song_id - 1, form.data)
-            songs.save_all()
-        return redirect(url_for("songs_list"))
-
-    return render_template("index_id.html", form=form, song_id=song_id, error=error)
-
-
-@app.errorhandler(400)
-def bad_request(error):
-    return make_response(jsonify({"error": "Bad request", "status_code": 400}), 400)
+@app.route("/update/<int:task_id>/", methods=["GET", "POST"])
+def update_task(song_id):
+    pass
 
 
 if __name__ == "__main__":
