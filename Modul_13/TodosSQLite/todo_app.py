b2255d9d92adbe5e46fdff3fb0132cd1be79c64f
Marcin <bizior.marcin@googlemail.com>
2021-01-18 12:37:41 +0100

    route updates

--- Modul_13/TodosSQLite/todo_app.py
+++ Modul_13/TodosSQLite/todo_app.py
@@ -60,23 +60,19 @@ def update_task(task_id):
     todo = c.fetchall()
     print(todo)
     print(task_id)
-    return render_template("/todo_id/", form=form, error=error)
-    # if request.GET.get("save", "").strip():
-    #    edit = request.GET.get("title", "").strip()
-    #
-    #    conn = sqlite3.connect("todos.db")
-    #    c = conn.cursor()
-    #    c.execute("UPDATE todo SET title = ?, description = ? WHERE id LIKE ?", (edit, no))
-    #    conn.commit()
-    #
-    #    return render_template("todos_id.html", form=form, error=error)
+    return render_template("/todo_id.html/", form=form, error=error)
+
+    title = request.form["title"]
+
+    c.execute("UPDATE todo SET title = ?, description = ? WHERE id LIKE ?", (edit, no))
+    conn.commit()
+    return render_template("todos_id.html", form=form, error=error)
     # else:
-    #    conn = sqlite3.connect("todos.db")
-    #    c = conn.cursor()
-    #    c.execute("SELECT task FROM todo WHERE id LIKE ?", (str(no)))
-    #    cur_data = c.fetchone()
-    #
-    #    return render_template("todos.html", form=form, error=error, todos=todos)
+    #   conn = sqlite3.connect("todos.db")
+    #   c = conn.cursor()
+    #   c.execute("SELECT task FROM todo WHERE id LIKE ?", (str(no)))
+    #   cur_data = c.fetchone()
+    #   return render_template("todos.html", form=form, error=error, todos=todos)
 
 
 if __name__ == "__main__":
