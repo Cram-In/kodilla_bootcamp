5b7aa89bcfba6d1377cd34b73e1cd000412d1dfb
Marcin <bizior.marcin@googlemail.com>
2021-01-19 11:08:29 +0100

    update set up

--- Modul_13/sql_todo/todo/main.py
+++ Modul_13/sql_todo/todo/main.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from config import app
 from models import Todos, get_all
-import sqlite3
 import uuid
+import sqlite3
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -43,24 +43,30 @@ def delete_task(task_id):
 
 @app.route("/update/<string:task_id>/", methods=["GET", "POST"])
 def update_task(task_id):
-    with sqlite3.connect("todo.db") as conn:
-        c = conn.cursor()
-        c.execute("SELECT * FROM todo")
-        tasks = c.fetchall()
-        print(task_id)
-        return render_template("todo_id.html")
-        title = request.form["title"]
-        description = request.form["description"]
-        print(title)
-        print(description)
-        print(task_id)
-        c.execute(
-            """UPDATE todo SET title = title, description = description
-                    WHERE task_id = taks_id""",
-            {"title": title, "description": description},
-        )
-        conn.commit()
-    return render_template("todo.html", task_id=task_id)
+    print(type(task_id))
+    print(task_id)
+    if request.method == "POST":
+        with sqlite3.connect("todo.db") as conn:
+            title = request.form["title"]
+            description = request.form["description"]
+            c = conn.cursor()
+            try:
+                c.execute(
+                    """UPDATE todo SET title = title, description = description
+                        WHERE task_id = taks_id""",
+                    {"title": title, "description": description},
+                )
+                conn.commit()
+                return redirect("/")
+            except:
+                return "There was an issue updating your task"
+
+    else:
+        with sqlite3.connect("todo.db") as conn:
+            c = conn.cursor()
+            c.execute("SELECT * FROM todo")
+            tasks = c.fetchall()
+        return render_template("todo_id.html", tasks=tasks)
 
 
 if __name__ == "__main__":
--- Modul_13/sql_todo/todo/sql_models.py
+++ Modul_13/sql_todo/todo/sql_models.py
@@ -7,9 +7,9 @@ c = conn.cursor()
 
 c.execute(
     """ CREATE TABLE todo (
-    task_id int AUTO_INCREMENT,
-    title text NOT NULL,
-    description text NOT NULL,
-    PRIMARY KEY (task_id)
+   task_id int AUTO_INCREMENT,
+   title text NOT NULL,
+   description text NOT NULL,
+   PRIMARY KEY (task_id)
 )"""
 )
