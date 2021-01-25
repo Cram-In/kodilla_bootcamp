10eb39dc557cab090db9a04511e995e21bb81c80
Marcin <bizior.marcin@googlemail.com>
2021-01-25 21:10:05 +0100

    upp

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///contacts.db"
 app.config["SECRET_KEY"] = "random string"
