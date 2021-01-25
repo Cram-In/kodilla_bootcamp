0b5bbf500bfd06250b8eff52124636aa092208a6
Marcin <bizior.marcin@googlemail.com>
2021-01-25 13:23:01 +0100

    make it better

---
+++ start.sh
@@ -0,0 +1,6 @@
+export FLASK_APP=main.py
+export FLASK_ENV=development
+set FLASK_ENV=development
+set FLASK_APP=main.py
+source /home/the-barbarian/PycharmProjects/Kodilla/kodilla_bootcamp/Modul_10/movies_project/movies_env/bin/activate
+flask run
